b455d6105b02e66029662e6d67271eca2d2a5ba2
haniemiresmaeili <haniemeer80@gmail.com>
2022-02-18 14:18:03 +0330

    fifth

---
+++ script.js
@@ -0,0 +1,11 @@
+let all = [];
+
+// get the div container
+function setup() {
+    let headerElement = document.getElementsByClassName("navbar")[0];
+
+    headerElement.innerHTML = "";
+function divContainer(){
+ let container=document.getElementsByClassName('container')
+
+}
